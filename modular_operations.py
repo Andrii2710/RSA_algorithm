982bdfdebbb5d0f03a03cc67332c584a28faf1d7
Andrii2710 <andriy.dvib@gmail.com>
2026-04-16 00:01:03 +0300

    implement basic modular operations

---
+++ modular_operations.py
@@ -0,0 +1,26 @@
+def euclidean_algorithm(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def extended_euclidean_algorithm(a, b):
+    if a == 0:
+        return b, 0, 1
+    gcd, x1, y1 = extended_euclidean_algorithm(b % a, a)
+    x = y1 - (b // a) * x1
+    y = x1
+    return gcd, x, y
+
+def mod_inverse(e, phi):
+    gcd, x, y = extended_euclidean_algorithm(e, phi)
+    if gcd != 1:
+        raise ValueError("Modular inverse does not exist")
+    return x % phi
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True

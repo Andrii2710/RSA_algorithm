c6a2960ab5d785fc1118c31479b0e2792c4081ac
Andrii2710 <andriy.dvib@gmail.com>
2026-04-16 00:23:06 +0300

    implement random selection of prime numbers and key generation

--- modular_operations.py
+++ modular_operations.py
@@ -40,3 +40,27 @@ def decrypt(private_key, cipher):
     d, n = private_key
     decrypted_chars = [chr(pow(char, d, n)) for char in cipher]
     return "".join(decrypted_chars)
+
+def get_random_primes():
+    primes = [101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157]
+    p = random.choice(primes)
+    q = random.choice(primes)
+    while p == q:
+        q = random.choice(primes)
+    return p, q
+
+def generate_keypair():
+    p, q = get_random_primes()
+
+    n = p * q
+    phi = (p - 1) * (q - 1)
+
+    e = 65537  #2^16+1
+    if euclidean_algorithm(e, phi) != 1:
+        e = 3
+        while euclidean_algorithm(e, phi) != 1:
+            e += 2
+
+    d = mod_inverse(e, phi)
+
+    return ((e, n), (d, n))
